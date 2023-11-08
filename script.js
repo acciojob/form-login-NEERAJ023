777c092dc27ef85037f883f7563215d78f270f14
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-08 17:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let form =document.querySelector("form");
+     let name=form.fname.value;
+	let last=form.lname.value;
+	alert(name+ " "+last);
 }
